adec914a4a0a2ac4133672a868685b18f81f24d5
unknown <joselitomartinho@gmail.com>
2020-08-08 23:50:14 -0300

    Add paginas e itens layout

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -8,10 +11,10 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="JoFlix Logo" />
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
