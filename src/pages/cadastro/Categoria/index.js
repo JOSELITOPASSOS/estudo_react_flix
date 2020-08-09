50c81a368c8db0602b4d7ddaca749366b812814e
unknown <joselitomartinho@gmail.com>
2020-08-09 00:30:15 -0300

    Melhorando o Carrocel usando componente Slider

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -7,6 +7,16 @@ function CadastroCategoria({ children }) {
       <PageDefault>
         <h1>Cadastro de Categoria</h1>
 
+        <form>
+          <label>
+            Nome da Categoria:
+            <input type="text" />
+          </label>
+          <button>
+            Cadastar
+          </button>
+        </form>
+
         <Link to="/">
           Ir para Home
         </Link>
